08449dcb561ee28fb38554a571c1e4fe9961ab0f
alberto.mila <alberto.mila@king.com>
2019-07-02 12:29:37 +0200

    First commit

---
+++ Cpp20-in-practice/Cpp20-in-practice.cpp
@@ -0,0 +1,11 @@
+// Cpp20-in-practice.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
---
+++ Cpp20-in-practice/ITest.h
@@ -0,0 +1,45 @@
+#pragma once
+
+////////////////////////////////////////////
+////Base class
+////////////////////////////////////////////
+class ITest
+{
+public:
+    virtual void Run() = 0;
+    virtual ~ITest() {}
+};
+
+
+////////////////////////////////////////////
+////Macro Run class
+////////////////////////////////////////////
+#define BEGIN_TEST(TestName)		\
+class TestName : public ITest		\
+{									\
+public:								\
+	virtual ~TestName() {}			\
+	virtual void Run() override		\
+	{
+
+#define END_TEST()					\
+	}								\
+};
+
+
+
+////////////////////////////////////////////
+////Macro Run global method
+////////////////////////////////////////////
+#define CONCAT(a,b) a ## b
+
+#define START_TEST_FOO(FunctionName)	\
+class CONCAT(Test_, FunctionName)		\
+{										\
+public:									\
+	CONCAT(Test_,FunctionName)()		\
+	{									\
+		g_mainPtr = &FunctionName;		\
+	}									\
+};										\
+CONCAT(Test_,FunctionName) CONCAT(g_,FunctionName);
